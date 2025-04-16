700ef8d65c851a51dca9d78bfeb59f633cb05512
“ColdNormal” <pengchun5789@outlook.com>
2025-04-16 22:22:14 +0800

    修补错误，修复了接口使用了泛型List<>但没有导包

--- src/main/java/com/example/demo/dao/StudentRepository.java
+++ src/main/java/com/example/demo/dao/StudentRepository.java
@@ -3,7 +3,10 @@ package com.example.demo.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface StudentRepository extends JpaRepository<Student,Long> {
 
+    List<Student> findByEmail(String email);    //这里不需要自己手写SQL，只有在复杂的查询条件才需要
 }
